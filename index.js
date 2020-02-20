3122f00564daf036697d2a25ec5dcd3edec1f7a8
uozgan <utkuozgan_8@hotmail.com>
2020-02-20 17:32:55 +0100

    new commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const app = express();
+
+app.get("/Funny", onRequest)
+
+function onRequest(request, response){
+  response.send(page);
+}
+
+const page = `<html>
+<head>
+<title>Utku Ozgan</title>
+</head>
+<body>
+<h1>What do you call a pig that does karate?</h1>
+
+<p>A pork chop</p>
+</body>
+</html>`
+
+const port = 3000;
+
+app.listen(port, confirmation);
+
+function confirmation(){
+  console.log(`Listening on: ${port}`)
+}
